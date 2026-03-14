9491a212f912a986dfc273f38905cef7789a67b7
antoniopaulo495 <antoniopaulodossantosneto5@gmail.com>
2026-03-14 10:35:28 -0300

    Initial commit

---
+++ 2º Funcionalidade/script_respon.js
@@ -0,0 +1,9 @@
+
+
+       function confirmar(){
+        const pais=document.getElementById('pais').value
+        const cpf=document.getElementById('cpf').value
+        const telefone=document.getElementById('telefone').value
+        const email1=document.getElementById('email1').value
+        const email2=document.getElementById('email2').value
+       }
---
+++ 3º funcionalidade/script_inclus_filhos.js
@@ -0,0 +1,8 @@
+  var alergia=document.getElementById("Alergias")
+        alergia.style.background="rgb(223, 145, 0)"
+         
+        function filhos_cadastrados(){
+            const nome_filho=document.getElementById("Nome")
+            const cpf_filho=document.getElementById("CPF")
+            const alergia_deficiencia=document.getElementById("Alergias").value
+        }
